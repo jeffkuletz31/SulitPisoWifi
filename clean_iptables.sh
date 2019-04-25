13d55add3676039ebb5804383f4865f209d53400
Rhalf Wendel David Caacbay <rhalfcaacbay@gmail.com>
2019-04-25 11:04:53 +0800

    Initial commit

---
+++ clean_iptables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+iptables -F
+iptables -X
+
+iptables -F -t nat
+iptables -X -t nat
+
+iptables -F -t mangle
+iptables -X -t mangle
+
---
+++ process.php
@@ -0,0 +1,17 @@
+<?php
+        if( isset( $_POST['ip'] ) && isset ( $_POST['mac'] ) ) {
+                $ip = $_POST['ip'];
+                $mac = $_POST['mac'];
+                exec("sudo iptables -I internet 1 -t mangle -m mac --mac-source $mac -j RETURN");
+                exec("sudo rmtrack " . $ip);
+                sleep(1);
+                // allowing rmtrack to be executed
+                // OK, redirection bypassed.
+                // Show the logged in message or directly redirect to other website
+                echo "User logged in.";
+        } else {
+                echo "Access Denied";
+        }
+?>
+
+
